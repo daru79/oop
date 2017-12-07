3b41e61876d2a7dc1643bcd7261346f9255bda64
daru79 <d.wojcicki@interia.pl>
2017-12-07 21:49:21 +0100

    end of episode 1-2-5

--- lib/BattleResult.php
+++ lib/BattleResult.php
@@ -6,7 +6,7 @@ class BattleResult
     private $winningShip;
     private $losingShip;
 
-    public function __construct($usedJediPowers, Ship $winningShip, Ship $losingShip)
+    public function __construct($usedJediPowers, Ship $winningShip = null, Ship $losingShip = null)
     {
         $this->usedJediPowers = $usedJediPowers;
         $this->winningShip = $winningShip;
@@ -22,7 +22,7 @@ class BattleResult
     }
 
     /**
-     * @return Ship
+     * @return Ship|null
      */
     public function getWinningShip()
     {
@@ -30,12 +30,18 @@ class BattleResult
     }
 
     /**
-     * @return Ship
+     * @return Ship|null
      */
     public function getLosingShip()
     {
         return $this->losingShip;
     }
 
-
+    /**
+     * @return bool
+     */
+    public function isThereAWinner()
+    {
+        return $this->getWinningShip() !== null;
+    }
 }
